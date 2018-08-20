ec5180abd4a358be46a539299a46fe298a1d64ea
Ryan Howe <rhowe@redhat.com>
2018-08-20 17:52:24 -0400

    first commit

---
+++ dns.py
@@ -0,0 +1,21 @@
+import socket
+import time
+import os 
+
+DNS_QUERY = os.getenv('DNS_QUERY', 'www.google.com')
+          
+def resolve(DNS_QUERY):
+i=0
+start_time = 0
+    try:
+        for i in range(10):
+            i = i + 1
+            start_time = time.time()
+            socket.getaddrinfo( DNS_QUERY, 443, 0, socket.SOCK_STREAM)
+    except Exception, e:
+        print "failure: %s (start time: %s, exception time: %s)" % (e,
+                str(start_time), str(time.time()))
+
+while True: 
+   resolve(DNS_QUERY)
+   time.sleep(5)
